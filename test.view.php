b987040c516ea28ddcd4957a068e4771328bbaeb
Diego Lusberg <diegolusberg97@gmail.com>
2021-04-12 21:21:45 -0400

    Interfaz de test agregada

---
+++ test.view.php
@@ -0,0 +1,149 @@
+<link rel="stylesheet" href="pregresp.css">
+<div class="pregresp">
+    <div class="pregunta">1. HUMOR ANSIOSO:¿Sientes Inquietud , Expectativas de catástrofe, Aprensión (anticipación temerosa) o Irritabilidad cuando te encuentras en tu lugar de trabajo?.<br />
+    </div>
+        <div class="respuestas">
+          <input type="radio" name="preg1" value="0" /> Ausente<br />
+          <input type="radio" name="preg1" value="1" /> Intensidad Ligera <br />
+          <input type="radio" name="preg1" value="2" /> Intensidad Media<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Elevada<br />
+          <input type="radio" name="preg1" value="4" /> Intensidad Máxima (Invalidante)<br />
+        </div>
+</div>
+
+  <div class="pregresp">
+    <div class="pregunta">2. TENSIÓN:¿Sientes Sensaciones de tensión, Fatigabilidad, Imposibilidad de estar quieto, Reacciones sobresalto,Llanto fácil,Temblores o Sensaciones de incapacidad para esperar cuando te solicitan una tarea?<br />
+    </div>
+        <div class="respuestas">
+          <input type="radio" name="preg1" value="1" /> Ausente<br />
+          <input type="radio" name="preg1" value="2" /> Intensidad Ligera <br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Media<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Elevada<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Máxima (Invalidante)<br />
+        </div>
+  </div>
+
+
+  <div class="pregresp">
+    <div class="pregunta">3. MIEDOS: ¿ Sientes muiedo a la oscuridad,a los desconocidos,a quedarse solo, a los animales. a, la circulación o a la muchedumbre cuando te encuentras en un entorno de trabajo?<br />
+    </div>
+        <div class="respuestas">
+          <input type="radio" name="preg1" value="1" /> Ausente<br />
+          <input type="radio" name="preg1" value="2" /> Intensidad Ligera <br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Media<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Elevada<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Máxima (Invalidante)<br />
+          </div>
+    </div>
+
+
+    <div class="pregresp">
+    <div class="pregunta">4. INSOMNIO: ¿ Sientes Dificultades de conciliación, Sueño interrumpido, Sueño no satisfactorio con cansancio al despertar,Sueños penosos, Pesadillas o Terrores nocturnos a menudo?<br />
+    </div>
+        <div class="respuestas">
+          <input type="radio" name="preg1" value="1" /> Ausente<br />
+          <input type="radio" name="preg1" value="2" /> Intensidad Ligera <br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Media<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Elevada<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Máxima (Invalidante)<br />
+          </div>
+    </div>
+
+    <div class="pregresp">
+    <div class="pregunta">5. FUNCIONES INTELECTUALES (COGNITIVAS): ¿Tienes Dificultad de concentración cuando realizas tareas o Mala memoria para recordar asuntos importantes?<br />
+    </div>
+        <div class="respuestas">
+          <input type="radio" name="preg1" value="1" /> Ausente<br />
+          <input type="radio" name="preg1" value="2" /> Intensidad Ligera <br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Media<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Elevada<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Máxima (Invalidante)<br />
+          </div>
+    </div>
+
+    <div class="pregresp">
+    <div class="pregunta">6. HUMOR DEPRESIVO: ¿Tienes Pérdida de interés por el trabajo, No disfrutas del tiempo libre, Depresión, Insomnio de madrugada o Variaciones anímicas a lo largo del día?<br />
+    </div>
+        <div class="respuestas">
+          <input type="radio" name="preg1" value="1" /> Ausente<br />
+          <input type="radio" name="preg1" value="2" /> Intensidad Ligera <br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Media<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Elevada<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Máxima (Invalidante)<br />
+          </div>
+    </div>
+
+    <div class="pregresp">
+    <div class="pregunta">7. SÍNTOMAS SOMÁTICOS MUSCULARES: ¿Sientes Dolores musculares, Rigidez o sacudidas musculares, Sacudidas clónicas, Rechinar de dientes o Voz quebrada?<br />
+    </div>
+        <div class="respuestas">
+          <input type="radio" name="preg1" value="1" /> Ausente<br />
+          <input type="radio" name="preg1" value="2" /> Intensidad Ligera <br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Media<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Elevada<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Máxima (Invalidante)<br />
+          </div>
+    </div>
+
+    <div class="pregresp">
+    <div class="pregunta">8. . SINTOMAS SOMÁTICOS GENERALES: ¿Tienes Zumbido de oídos. Visión borrosa. Oleadas de calor o frio. Sensación de debilidad. Parestesia (pinchazos u hormigueos)?<br />
+    </div>
+        <div class="respuestas">
+          <input type="radio" name="preg1" value="1" /> Ausente<br />
+          <input type="radio" name="preg1" value="2" /> Intensidad Ligera <br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Media<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Elevada<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Máxima (Invalidante)<br />
+          </div>
+    </div>
+
+    <div class="pregresp">
+    <div class="pregunta">9. SINTOMAS CARDIOVASCULARES:¿Tienes Taquicardia. Palpitaciones. Dolor torácico, Sensación pulsátil en vasos, Sensaciones de baja presión o desmayos. Extrasístoles (arritmias cardiacas benignas)?<br />
+    </div>
+        <div class="respuestas">
+          <input type="radio" name="preg1" value="1" /> Ausente<br />
+          <input type="radio" name="preg1" value="2" /> Intensidad Ligera <br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Media<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Elevada<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Máxima (Invalidante)<br />
+          </div>
+    </div>
+
+    <div class="pregresp">
+    <div class="pregunta">10. SINTOMAS RESPIRATORIOS:¿Sientes Opresión pretorácica. Constricción precordial. Sensación de ahogo falta de aire. Suspiros. Disnea (dificultad para respirar) cuando te dan responsabilidades?.<br />
+    </div>
+        <div class="respuestas">
+          <input type="radio" name="preg1" value="1" /> Ausente<br />
+          <input type="radio" name="preg1" value="2" /> Intensidad Ligera <br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Media<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Elevada<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Máxima (Invalidante)<br />
+          </div>
+    </div>
+
+    <div class="pregresp">
+    <div class="pregunta">11. SINTOMAS GASTROINTESTINALES:¿Tienes Dificultades evacuatorias,Gases, dolores o ardor antes o después de comer, hinchazón abdominal, náuseas, vómitos, Cólicos(espasmos) abdominales, diarrea. pérdida de peso. Estreñimiento constantemente?<br />
+    </div>
+        <div class="respuestas">
+          <input type="radio" name="preg1" value="1" /> Ausente<br />
+          <input type="radio" name="preg1" value="2" /> Intensidad Ligera <br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Media<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Elevada<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Máxima (Invalidante)<br />
+          </div>
+    </div>
+
+    <div class="pregresp">
+    <div class="pregunta">12. SÍNTOMAS GENITOURINARIOS: ¿Tienes micciones frecuentes, micción imperiosa, amenorrea (falta del periodo menstrual) de manera habitual?<br />
+    </div>
+        <div class="respuestas">
+          <input type="radio" name="preg1" value="1" /> Ausente<br />
+          <input type="radio" name="preg1" value="2" /> Intensidad Ligera <br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Media<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Elevada<br />
+          <input type="radio" name="preg1" value="3" /> Intensidad Máxima (Invalidante)<br />
+          </div>
+    </div>
+
+
+
